Fixed power calculatins in altitude analysis by adding propulsive efficiency and having correct units for incidence angle. Added the option to plot power vs refuel time

AltitudeAnalysis/altitude_analysis.py
@@ -31,7 +31,7 @@ import os
 # Add the parent directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
-from plot_performance import plot_time_vs_massflow, plot_power_vs_altitude
+from plot_performance import plot_time_vs_massflow, plot_power_vs_altitude, plot_power_vs_time
 from classes import Regime
 from spacecraft import spacecraft
 
@@ -158,11 +158,11 @@ def Get_PowReq(Regime:Regime, sc_parameters:dict) -> np.ndarray:
     m_out = Getm_gain(Regime, sc_parameters)[2]
 
     # Calculate the minimum power required
-    P_req_prop = 0.5 * m_out * Regime.v_circ()**2
+    P_req_prop = 0.5 * m_out * Regime.v_circ()**2 / sc_parameters['n_prop']
 
     # Calculate the solar panel area required
     Flux = Regime.earth_params['SolarFlux']
-    A_solar = P_req_prop / eff_sol / Flux / np.cos(67.5)  # cosine comes from average incidence angle over an orbit
+    A_solar = P_req_prop / eff_sol / Flux / np.cos(np.deg2rad(67.5))  # cosine comes from average incidence angle over an orbit
 
     return P_req_prop, A_solar
 
@@ -179,10 +179,10 @@ if __name__ == '__main__':
     # Calculate the mass flow rate gain
     m_gain, m_in, m_out = Getm_gain(regime, spacecraft)
 
-    # Normalize with respoect to intake area, and convert to kg/hour
-    m_gain = m_gain / spacecraft['A_intake'] * 3600
-    m_in = m_in / spacecraft['A_intake'] * 3600
-    m_out = m_out / spacecraft['A_intake'] * 3600
+    # Convert to kg/hour
+    m_gain = m_gain * 3600
+    m_in = m_in * 3600
+    m_out = m_out * 3600
 
     # Calculate the minimum altitudes
     h_drag, h_heat = Get_minAlts(regime, spacecraft)
@@ -202,11 +202,13 @@ if __name__ == '__main__':
     m_crop = m_gain[mask]
     h_crop = h[mask]  # Need to filter h too
 
-    # Find the max mass flow rate and corresponding height
+    # Find the max mass flow rate and corresponding height and time of refueling
     if len(m_crop) > 0:  # Ensure there are valid values
         idx_max = np.argmax(m_crop)
         h_max = h_crop[idx_max]
         m_max = m_crop[idx_max]
+        time_min = tank_load / m_max / 24  # Time in days
+        P_min = P_req[np.where(h == h_max)][0]  # Minimum power required
     else:
         h_max = None
         m_max = None  # Handle the case where no valid values exist
@@ -214,7 +216,10 @@ if __name__ == '__main__':
     # Print the results
     print('The maximum mass flow rate gain is:', m_max * 3600, 'kg/hour')
     print('The altitude at which this occurs is:', h_max, 'km')
+    print('The minimum time to refuel is:', time_min, 'days')
+    print('The minimum power required is:', P_min, 'W')
 
     # Plot the results
     plot_time_vs_massflow(h, m_gain, m_in, m_out, time, h_drag, h_heat)
     plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat)
+    plot_power_vs_time(P_req, time, time_min)

AltitudeAnalysis/plot_performance.py
@@ -1,74 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from cycler import cycler
+import sys
+import os
 
-# Set plotting preferences
-# These cyclers are basically color and line themes for the plots
-cycl_std = (cycler(color=['#0C5DA5', '#00B945', '#FF9500', '#FF2C00', '#845B97', '#474747', '#9e9e9e'])
-         + cycler(linestyle=['-', '--', '-.', ':', '-', '--', '-.'])) # You need to have as many colors as linestyles
+# Add the parent directory to the path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
-cycl_ibm = (cycler(color=['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'])
-         + cycler(linestyle=['-', '--', '-.', ':', '-'])) # You need to have as many colors as linestyles
-
-cycl_deep = (cycler(color=['#1f3b73', '#1f6b3b', '#8b1e3f', '#d35400', '#5e2a84', '#704214', '#b8860b', '#116466', '#a83279', '#4b4e57', '#5d3a9b', '#008b8b', '#4b830d', '#d3545d', '#3c3f8f', '#c44536', '#0e6655', '#a67c00'])
-         + cycler(linestyle=['-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--'])) # You need to have as many colors as linestyles
-
-cycl_pastel = (cycler(color=['#6c8ebf', '#8fbf6c', '#bf6c6c', '#e5a46c', '#b07fb0', '#c4a484', '#f2d16d', '#76b7b2', '#f3a6b3', '#b0b7c6', '#c2a2da', '#91d8e4', '#b7de79', '#f7c59f', '#7c83bc', '#f4a988', '#a4d4ae', '#e4b363'])
-         + cycler(linestyle=['-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--'])) # You need to have as many colors as linestyles
-
-# Update the plot preferences (you can change these to your liking)
-plt.rcParams.update({
-
-    # General
-    'font.size': 16,
-    'figure.figsize': (12, 7),
-
-    # Grid
-    'axes.grid': True,
-    'grid.color': 'gray',
-    'grid.linestyle': '--',
-    'grid.linewidth': 0.5,
-    'grid.alpha': 0.7,
-
-    # Fonts
-    'font.family': 'serif',
-    # 'font.serif': ['Times New Roman'],
-
-    # Colors and line styles
-    'axes.prop_cycle' : cycl_ibm,
-    'lines.linewidth': 2,
-
-    # Legend
-    'legend.frameon': False,
-
-    # Set x axis
-    'xtick.major.size' : 3,
-    'xtick.major.width' : 0.5,
-    'xtick.minor.size' : 1.5,
-    'xtick.minor.width' : 0.5,
-    'xtick.minor.visible' : True,
-    'xtick.top' : True,
-
-    # Set y axis
-    'ytick.major.size' : 3,
-    'ytick.major.width' : 0.5,
-    'ytick.minor.size' : 1.5,
-    'ytick.minor.width' : 0.5,
-    'ytick.minor.visible' : True,
-    'ytick.right' : True,
-
-    # Handle latex text
-    'text.usetex': False,  # Ensures no external LaTeX installation is used
-    'mathtext.fontset': 'dejavuserif',  # Ensures math text matches the rest of the text
-    'font.family': 'serif',  # Match the font with your main font
-    # 'font.serif': ['Times New Roman'],  # Change if needed
-
-})
+import mpl_style
 
 
 # Plotting functions
 
-def plot_time_vs_massflow(h, m_gain, m_in, m_out, time, h_drag, h_heat, SAVEPATH=None):
+def plot_time_vs_massflow(h:np.ndarray, m_gain:np.ndarray, m_in:np.ndarray, m_out:np.ndarray, time:np.ndarray,
+                           h_drag:float, h_heat:float, SAVEPATH:str=None):
     # Create the plot
     fig, ax1 = plt.subplots()
 
@@ -80,7 +24,7 @@ def plot_time_vs_massflow(h, m_gain, m_in, m_out, time, h_drag, h_heat, SAVEPATH
  
     ax1.set_yscale('log')
     ax1.set_xlabel('Altitude ($km$)')
-    ax1.set_ylabel('Mass flow rate ($kg/hour/m^2$)')
+    ax1.set_ylabel('Mass flow rate ($kg/hour$)')
     ax1.tick_params(axis='y')
     ax1.tick_params(axis='x')
 
@@ -88,7 +32,7 @@ def plot_time_vs_massflow(h, m_gain, m_in, m_out, time, h_drag, h_heat, SAVEPATH
     ax2 = ax1.twinx()
     ax2.plot(h, time, label='Refueling time', color='#FE6100', linestyle=':')
     ax2.set_yscale('log')
-    ax2.set_ylabel('Time to refuel ($days/m^2$)')
+    ax2.set_ylabel('Time to refuel ($days$)')
     ax2.tick_params(axis='y')
 
     # Add vertical lines for the minimum altitudes
@@ -108,8 +52,9 @@ def plot_time_vs_massflow(h, m_gain, m_in, m_out, time, h_drag, h_heat, SAVEPATH
         plt.show()
 
 
-def plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat, SAVEPATH=None):
-# Create the plot for the power requirements
+def plot_power_vs_altitude(h:np.ndarray, P_req:np.ndarray, A_solar:np.ndarray,
+                            h_drag:float, h_heat:float, SAVEPATH:str=None):
+    # Create the plot for the power requirements
     fig, ax1 = plt.subplots()
 
     # Plot mass flow rates on the left y-axis
@@ -117,7 +62,7 @@ def plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat, SAVEPATH=None):
 
     ax1.set_yscale('log')
     ax1.set_xlabel('Altitude (km)')
-    ax1.set_ylabel('Required power (W)')
+    ax1.set_ylabel('Required power $(W)$')
     ax1.tick_params(axis='y')
 
     # Create the second y-axis
@@ -125,7 +70,7 @@ def plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat, SAVEPATH=None):
     ax2.plot(h, A_solar, label='Required solar panel area')
     ax2.set_yscale('log')
 
-    ax2.set_ylabel('Required solar panel area (m^2)')
+    ax2.set_ylabel('Required solar panel area $(m^2)$')
     ax2.tick_params(axis='y')
 
     # Add lines for the minimum altitudes
@@ -134,8 +79,8 @@ def plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat, SAVEPATH=None):
     # ax1.axvline(h_max, color='k', linestyle='--', label='Altitude of max gain')
 
     # Legends
-    ax1.legend(loc='upper left', bbox_to_anchor=(0.6, 0.75))
-    ax2.legend(loc='upper left', bbox_to_anchor=(0.6, 0.45))
+    ax1.legend(loc='upper left', bbox_to_anchor=(0.55, 0.95))
+    ax2.legend(loc='upper left', bbox_to_anchor=(0.55, 0.75))
 
     plt.title('Power Requirements vs Altitude')
     plt.grid()
@@ -147,4 +92,32 @@ def plot_power_vs_altitude(h, P_req, A_solar, h_drag, h_heat, SAVEPATH=None):
         plt.show()
 
 
+def plot_power_vs_time(P_req:np.ndarray, time:np.ndarray,
+                        t_min:float, SAVEPATH=None):
+    # Create the plot for the power requirements
+    fig, ax1 = plt.subplots()
+
+    # Plot time
+    ax1.plot(time, P_req, label='Power vs Time')
+
+    ax1.set_yscale('log')
+    ax1.set_xscale('log')
+    ax1.set_xlabel('Time $(days)$')
+    ax1.set_ylabel('Required power $(W)$')
+    ax1.tick_params(axis='y')
+    ax1.tick_params(axis='x')
+
+    # Add lines for the minimum altitudes
+    ax1.axvline(t_min, color='magenta', linestyle='--', label='Minimum refueling time')
+
+    # Legends
+    ax1.legend(loc='upper left', bbox_to_anchor=(0.55, 0.95))
+
+    plt.title('Power Requirements vs Time')
+
+    if SAVEPATH is not None:
+        plt.savefig(SAVEPATH)
+    
+    else:
+        plt.show()
 